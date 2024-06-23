434c3bffafd6530a3843aecaba11c20ffbf5e5db
Михаил Лубенцов <wispergently061@gmail.com>
2024-06-23 20:44:54 +0500

    Инициализировал репозиторий

---
+++ Intro_Abstraction_and_Interface.java
@@ -0,0 +1,10 @@
+import static print_module.print_library.println;
+/**
+ * Intro_Abstraction_and_Interface
+ */
+public class Intro_Abstraction_and_Interface {
+
+    public static void main(String[] args) {
+        println("Hello");
+    }
+}
